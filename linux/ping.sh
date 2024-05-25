89ccd87e928475ea34fef90aea41c6bb0b0ca83a
Ahmed Gomaa <gomaaa447@gmai.com>
2024-05-25 12:50:41 +0300

    linux labs

---
+++ linux/ping.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+subnet="192.168.1"
+## scripting is now ruuning 
+for ip in {1..255}
+do
+    if ping -c 1 -W 1 ${subnet}.${ip} &> /dev/null; then
+        echo "Host ${subnet}.${ip} is up"
+    else   
+        echo "Host ${subnet}.${ip} is down" 
+    fi
+done
